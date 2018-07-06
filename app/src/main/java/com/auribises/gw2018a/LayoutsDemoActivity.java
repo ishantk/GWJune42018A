a5793d3f9d1ce7f57fb4f0ff7dfd1ee2814c6516
Ishant Kumar <er.ishant@gmail.com>
2018-07-06 11:33:08 +0530

    Android Layouts

---
+++ app/src/main/java/com/auribises/gw2018a/LayoutsDemoActivity.java
@@ -0,0 +1,15 @@
+package com.auribises.gw2018a;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LayoutsDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layouts_demo);
+
+        //Math.sin()
+    }
+}
